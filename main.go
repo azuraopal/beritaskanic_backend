48086eafb3de879ef5eb11dd4871fbbee6c80fb2
ValSpp <muhammadnovals334@gmail.com>
2024-03-03 12:39:28 +0700

    add: connection to database

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ValSpp/ber1taskanic/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+
+	port := os.Getenv("PORT")
+	app := fiber.New()
+	app.Listen(":" + port)
+
+}
