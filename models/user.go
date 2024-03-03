443242fcb614be321ad222362909615c84e44978
ValSpp <muhammadnovals334@gmail.com>
2024-03-03 14:48:17 +0700

    add: struct users

---
+++ models/user.go
@@ -0,0 +1,17 @@
+package models
+
+import "golang.org/x/crypto/bcrypt"
+
+type User struct {
+	Id        uint   `json:"id"`
+	FirstName string `json:first_name`
+	LastName  string `json:last_name`
+	Email     string `json:email`
+	Password  []byte `json:"-"`
+	Phone     string `json:"phone"`
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	user.Password = hashedPassword
+}
