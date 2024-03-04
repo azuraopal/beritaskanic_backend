59441d7959675d717108d0f02a817c53226b4902
ValSpp <muhammadnovals334@gmail.com>
2024-03-04 19:46:04 +0700

    add: route for postingan and Middleware

--- routes/route.go
+++ routes/route.go
@@ -2,12 +2,15 @@ package routes
 
 import (
 	"github.com/ValSpp/ber1taskanic/controller"
+	"github.com/ValSpp/ber1taskanic/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
-	//app.Use(middleware.IsAuthenticate)
+	app.Use(middleware.IsAuthenticate)
+
+	app.Post("/api/post", controller.CreatePost)
 
 }
