8fda1bcb225d0c7bdd95157355717accda09c51c
ValSpp <muhammadnovals334@gmail.com>
2024-03-05 07:57:09 +0700

    feat: Allpost

--- controller/postController.go
+++ controller/postController.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/ValSpp/ber1taskanic/database"
 	"github.com/ValSpp/ber1taskanic/models"
@@ -24,6 +26,20 @@ func CreatePost(c *fiber.Ctx) error {
 	})
 }
 
-func AllPost(c *fiber.Ctx) {
-	
+func AllPost(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 5
+	offset := (page - 1) * limit
+	var total int64
+	var ambilberita []models.Berita
+	database.DB.Preload("User").Offset(offset).Limit(limit).Find(&ambilberita)
+	database.DB.Model(&models.Berita{}).Count(&total)
+	return c.JSON(fiber.Map{
+		"data": ambilberita,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(int(total) / limit)),
+		},
+	})
 }
