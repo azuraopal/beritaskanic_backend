926277c4cf0891efda9ddc39214de934175515f9
ValSpp <muhammadnovals334@gmail.com>
2024-03-05 13:11:09 +0700

    add: router for uniquePost

--- routes/route.go
+++ routes/route.go
@@ -15,5 +15,6 @@ func Setup(app *fiber.App) {
 	app.Get("/api/allpost", controller.AllPost)
 	app.Get("/api/allpost/:id", controller.DetailPost)
 	app.Put("/api/updatepost/:id", controller.UpdatePost)
+	app.Get("/api/uniquepost", controller.UniquePost)
 
 }
