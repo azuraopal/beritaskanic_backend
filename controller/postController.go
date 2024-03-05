b9ee84fd34e798e5a1773eb1d71dc56ef8b523ce
ValSpp <muhammadnovals334@gmail.com>
2024-03-05 17:53:50 +0700

    fix: error Update post and Delete Post

--- controller/postController.go
+++ controller/postController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"strconv"
@@ -9,6 +10,7 @@ import (
 	"github.com/ValSpp/ber1taskanic/models"
 	"github.com/ValSpp/ber1taskanic/util"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 func CreatePost(c *fiber.Ctx) error {
@@ -59,8 +61,15 @@ func UpdatePost(c *fiber.Ctx) error {
 	berita := models.Berita{
 		Id: uint(id),
 	}
-	if err := c.BodyParser(&berita); err != nil {
-		fmt.Println("Unable to parse body")
+	// if err := c.BodyParser(&berita); err != nil {
+	// 	fmt.Println("Unable to parse body")
+	// }
+	database.DB.Where("id=?", id).Preload("User").First(&berita)
+	if berita.Id == 0 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Gagal Mengupdate, Silahkan coba lagi!",
+		})
 	}
 	database.DB.Model(&berita).Updates(berita)
 	return c.JSON(fiber.Map{
@@ -75,4 +84,34 @@ func UniquePost(c *fiber.Ctx) error {
 	database.DB.Model(&berita).Where("user_id=?", id).Preload("User").Find(&berita)
 
 	return c.JSON(berita)
+}
+
+func DeletePost(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	berita := models.Berita{
+		Id: uint(id),
+	}
+
+	var postberita models.Berita
+	database.DB.Where("id=?", id).Preload("User").First(&postberita)
+	if postberita.Id == 0 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Maaf, Postingan tidak ada",
+		})
+	}
+
+	deleteQuery := database.DB.Delete(&berita)
+
+	if errors.Is(deleteQuery.Error, gorm.ErrRecordNotFound) {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Maaf, Postingan tidak ada",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Postingan berhasil dihapus!",
+	})
+
 }
